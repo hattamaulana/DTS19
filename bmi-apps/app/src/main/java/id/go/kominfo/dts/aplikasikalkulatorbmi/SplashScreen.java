f8181a2cc2031c397a282b1903c7fe29010ee095
HattaMaulana <mahatta.maulana@gmail.com>
2019-07-10 06:57:11 +0700

    Bab 06 Praktikum 2 View

---
+++ bmi-apps/app/src/main/java/id/go/kominfo/dts/aplikasikalkulatorbmi/SplashScreen.java
@@ -0,0 +1,40 @@
+package id.go.kominfo.dts.aplikasikalkulatorbmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            // Get Window Android
+            Window window = getWindow();
+
+            // Set Fullscreen
+            // change status bar color to transparent
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
